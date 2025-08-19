ab2b7cad4260177a6651dc2468e3c4690181c99e
Illeyan Atif <illeyan.atif@gmail.com>
2025-08-19 14:13:52 -0700

    utilities

--- myvector.h
+++ myvector.h
@@ -1,3 +1,6 @@
+#ifndef MYVECTOR_H
+#define MYVECTOR_H
+
 #include <iostream>
 #include <stdexcept>
 
@@ -7,6 +10,8 @@ private:
     T* data;
     size_t _size;
     size_t _capacity;
+
+    void resize(size_t capacity);
 
 
 public:
@@ -23,4 +28,35 @@ public:
     MyVector& operator=(const MyVector& other); // copy assignment
 
     MyVector& operator=(MyVector&& other); //move assignment
+
+    //element access
+    T& operator[](size_t index);
+    const T& operator(size_t index);
+    T& at(size_t index);
+    const T& at(size_t index) const;
+    T& front;
+    T& back;
+
+    //capacity
+    size_t size() const;
+    size_t capacity() const;
+    bool empty() const;
+
+    //modifiers
+    void push_back(const T& value);
+    void push_back(T&& value);
+    void pop_back();
+    void clear();
+    void reserve(size_t new_cap);
+    void resize(size_t new_size, const T& value = T());
+
+    //iterators
+    T* begin();
+    T* end();
+    const T* begin() const;
+    const T* end() const;
 }
+
+#include "myvector.ipp"
+
+#endif
